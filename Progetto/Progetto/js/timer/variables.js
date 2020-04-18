dd297410c57bb668511e30b9cf25eb6b0f497bb1
Donuts7 <56405823+Donuts7@users.noreply.github.com>
2020-04-18 21:55:03 +0200

    Add files via upload

---
+++ Progetto/Progetto/js/timer/variables.js
@@ -0,0 +1,27 @@
+const title = document.querySelector('.sessionName')
+const timer = document.querySelector('.timer')
+const brkTimer = document.querySelector('.break')
+
+// add timer minutes
+const minusMin = document.querySelector('.minus')
+const addMin = document.querySelector('.add')
+
+// timer 
+const play = document.querySelector('.play')
+const stop = document.querySelector('.stop')
+const pause = document.querySelector('.pause')
+
+// break buttons
+const minusBrk = document.querySelector('.minusBrk')
+const addBrk = document.querySelector('.addBrk')
+
+const addButton = document.querySelector('.btnAdd')
+
+const audio = document.querySelector('.audio')
+
+const tik = document.querySelector('audio[data-key="tik"]');
+const go = document.querySelector('audio[data-key="go"]');
+const finish = document.querySelector('audio[data-key="finish"]');
+
+const marker = document.querySelector('.inner')
+
