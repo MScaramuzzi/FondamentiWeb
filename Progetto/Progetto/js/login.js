88746e75a58a9402e70011c8d9ae40ead72bce83
Donuts7 <56405823+Donuts7@users.noreply.github.com>
2020-04-25 19:19:55 +0200

    Add files via upload

--- Progetto/Progetto/js/login.js
+++ Progetto/Progetto/js/login.js
@@ -1,15 +1,22 @@
-var x = document.getElementById("login");
-var y = document.getElementById("register");
-var z = document.getElementById("btn");
+//si ottengono le variabili dalla DOM
 
-        function register() {
-            x.style.left = "-400px";
-            y.style.left = "50px";
-            z.style.left = "110px";
-        }
+const x = document.getElementById("login");
+const y = document.getElementById("register");
+const z = document.getElementById("btn");
 
-        function login() {
-            x.style.left = "50px";
-            y.style.left = "450px";
-            z.style.left = "0";
-        }
+
+/**  dichiara le due funzioni che permettono al form di registrazione
+ di slittare a destra e a sinistra in base al fatto se si clicca sul bottone
+ login o register
+*/
+function register() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0";
+}
