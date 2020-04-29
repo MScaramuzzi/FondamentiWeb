d68b4ff606cd527731e71d47af71103659b44f91
Antonello <58037244+Elvale98@users.noreply.github.com>
2020-04-29 20:45:53 +0200

    Update todo.js

--- Progetto/js/todo.js
+++ Progetto/js/todo.js
@@ -1,8 +1,8 @@
 // Elementi della pagina che dovranno essere selezionati
 const clear = document.querySelector(".clear");
 
-/** Rende la data più leggeibile trasformando  
- * la prima lettera di giorno e mese in maiuscolo */
+/* Rende la data più leggeibile trasformando  
+   la prima lettera del giorno e del mese corrente in maiuscolo */
 
 
 document.getElementById("date").style.textTransform = "capitalize";
@@ -12,18 +12,18 @@ const input = document.getElementById("input");
 const add = document.querySelector(".task");
 
 //  Nomi delle classi
-const CHECK = "fa-check-circle";
-const UNCHECK = "fa-circle-thin";
-const LINE_THROUGH = "lineThrough";
+const CHECK = "fa-check-circle"; //check effettuato (attività completata)
+const UNCHECK = "fa-circle-thin"; //check non effettuato (attività da completare)
+const LINE_THROUGH = "lineThrough"; //linea per sbarrare attività completata
 
 // Variabili
 let LIST, id;
 
 // ottieni l'item dal local storage
-//spiega getItem
-let data = localStorage.getItem("TODO");
+//Il metodo getItem() restituisce il valore contenuto nella chiave passata a parametro (TODO).
+let data = localStorage.getItem("TODO"); //ripristina il vettore LIST
 
-// controlla che data non sia vuoto
+// controlla che la data non sia vuoto
 if (data) {
     LIST = JSON.parse(data);
     id = LIST.length; // setta l'id dell'elemento come ultimo della list
@@ -43,13 +43,12 @@ function loadList(array) {
 }
 
 //ripulisci local storage
-//spiega clear e reload,location
 clear.addEventListener("click", function() {
-    localStorage.clear();
-    location.reload();
+    localStorage.clear(); //cliccando l'icona il alto a destra (clear) si elimina i contento della To Do List
+    location.reload();//essendo stata eliminata la To Do List, ricaricando la pagina risult essere ancora vuota
 });
 
-//  Mostra data di oggi
+// Mostra la data di oggi
 const options = { weekday: "short", month: "long", day: "numeric" };
 const today = new Date();
 
@@ -57,15 +56,14 @@ const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("it-IT", options);
 
 
-
-
-
+//aggiungi attività alla lista
 function addToDo(toDo, id, done, trash) {
 
     if (trash) { return; }
 
-    const DONE = done ? CHECK : UNCHECK;
-    const LINE = done ? LINE_THROUGH : "";
+
+    const DONE = done ? CHECK : UNCHECK; //se l'attività è completata si aggiunge la spunta
+    const LINE = done ? LINE_THROUGH : ""; //se l'attività è completata viene sbarrata
 
     const item = `<li class="item">
                     <i class="fa ${DONE} co" job="complete" id="${id}"></i>
@@ -74,19 +72,23 @@ function addToDo(toDo, id, done, trash) {
                   </li>
                 `;
 
+    //costante dove è definita la posizione in cui vanno aggiunte le attività alla lista
     const position = "beforeend";
 
+    /*si occupa di aggiungere le varie attività una sotto l'altra partendo dall'avere
+    in cima l'attività meno recentemente aggiunta e le attività più recenti a seguire*/
     list.insertAdjacentHTML(position, item);
 }
 
 // Aggiungi un elemento alla lista quando l'utente preme enter
-// il codice 13 è quello di enter
+// il codice 13 è specifico proprio del tasto enter 
 document.addEventListener("keyup", function(event) {
     if (event.keyCode == 13) {
         const toDo = input.value;
 
         //se l'input non è vuoto
         if (toDo) {
+            //in caso affermativo aggiunge l'attività alla lista
             addToDo(toDo, id, false, false);
 
             LIST.push({
@@ -99,12 +101,16 @@ document.addEventListener("keyup", function(event) {
             /* aggiungi item al localstorage (questo codice deve essere presente 
             in ogni punto del codice dove il vettore LIST è modificato) */
 
-            localStorage.setItem("TODO", JSON.stringify(LIST)); //vedi JSON.stringify
-            //spiega setItem
-
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+            /*il metodo localStorage.setItem() quando passa il nome di una chiave e un valore, aggiunge questa chiave allo storage, 
+            oppure se la chiave esiste già aggiorna il valore della chiave stessa.*/
+            /*il metodo JSON.stringify converte un oggetto o un valore JavaScript in una stringa JSON, sostituendo facoltativamente i 
+            valori se viene specificata una funzione sostitutiva o facoltativamente includendo solo le 
+            proprietà specificate se viene specificato un array replacer.*/
 
             id++;
         }
+        //di conseguenza viene "pulito" l'input
         input.value = "";
     }
 });
@@ -122,7 +128,7 @@ add.addEventListener("click", function(event) {
             LIST.push({
                 name: toDo,
                 id: id,
-                done: false,
+                done: false, //attività non completata
                 trash: false
             });
 
@@ -138,19 +144,23 @@ add.addEventListener("click", function(event) {
 
 
 // completa il to do item
-//spiega classlist e toggle
 function completeToDo(element) {
-    element.classList.toggle(CHECK);
-    element.classList.toggle(UNCHECK);
-    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+    /* La proprietà Element.classList di sola lettura restituisce una raccolta dinamica delle classi dell'elemento.
+    Con il metodo toggle si aggiunge o rimuove il valore della classe (se la classe esiste, la rimuove e 
+    restituisce false, altrimenti l'aggiunge e regsituisce true*/
+
+    element.classList.toggle(CHECK); //se il check è stato fatto allora rimuovilo
+    element.classList.toggle(UNCHECK);//se l'uncheck è impostato allora rimuovilo
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);//seleziona il testo dell'attività e a questa aggiunge una linea orizzontale
 
     LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
 // rimuovi to do item
-// spiega parentnode
+// parentNode è il genitore del nodo corrente. Il genitore di un elemento è un nodo Element
 function removeToDo(element) {
-    element.parentNode.parentNode.removeChild(element.parentNode);
+    element.parentNode.parentNode.removeChild(element.parentNode);//al click dell'iconda del cestino, elimina la riga contentenente la stringa 
+                                                                  //dell'attività (1° parentNode) e il box che la contiene (2°parentNode)
 
     LIST[element.id].trash = true;
 }
@@ -161,6 +171,8 @@ list.addEventListener("click", function(event) {
     const element = event.target; // ritorna l'elemento clickato all'interno della lista 
     const elementJob = element.attributes.job.value; // completa o elimina
 
+    //verifica se l'attività è completata e richiama la funzione completeToDo()
+    //altrimenti se l'attività è eliminata, allora si richiama la funzione removeToDo()
     if (elementJob == "complete") {
         completeToDo(element);
     } else if (elementJob == "delete") {
